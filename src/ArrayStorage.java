0e5e7c874e5f126597f5752d9621fe61923dcdf0
Bystrov Daniil <bstrdn@gmail.com>
2020-03-12 12:51:59 +0300

    реализация метода

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,30 +1,56 @@
+import java.sql.Array;
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-    Resume[] storage = new Resume[10000];
+    private Resume[] storage = new Resume[10000];
+    private int storageNumb = 0;
 
     void clear() {
-    }
+        for (int i = 0; i < storageNumb; i++) {
+            storage[i] = null;
+            }
+        storageNumb = 0;
+        }
 
     void save(Resume r) {
+        storage[storageNumb] = r;
+        storageNumb++;
     }
 
     Resume get(String uuid) {
+        for (int i = 0; i < storageNumb; i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                return storage[i];
+            }
+        }
+
         return null;
     }
 
     void delete(String uuid) {
+        for (int i = 0; i < storageNumb; i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                storage[i] = storage[storageNumb-1];
+                storage[storageNumb-1] = null;
+                storageNumb--;
+            }
+        }
+
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        Resume[] resumes = new Resume[storageNumb];
+        resumes = Arrays.copyOf(storage, storageNumb);
+        return resumes;
     }
 
     int size() {
-        return 0;
+        return storageNumb;
     }
 }
