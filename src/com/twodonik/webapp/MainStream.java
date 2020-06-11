13d53a78a9f61dc2a4dbba10c5c6dc238de800bc
Bystrov Daniil <bstrdn@gmail.com>
2020-06-11 15:06:17 +0300

    HW_12.1

--- src/com/twodonik/webapp/MainStream.java
+++ src/com/twodonik/webapp/MainStream.java
@@ -11,20 +11,21 @@ public class MainStream {
         int[] val = {12, 2, 3, 8, 7, 53, 4, 4, 1, 2};
         System.out.println(minValue(val));
 
-        List<Integer> integers = oddOrEven(Arrays.asList(12, 34, 2, 546, 1, 2, 3, 1));
+        List<Integer> integers = oddOrEven(Arrays.asList(1, 2, 3, 4, 5, 6));
         System.out.println(integers);
     }
 
-
     static int minValue(int[] values) {
-        return IntStream.of(values).filter((x) -> x < 10 && x > 0).sorted().distinct().reduce(0, (n, p) -> n * 10 + p);
+        return IntStream.of(values)
+                .filter((x) -> x < 10 && x > 0)
+                .distinct()
+                .sorted()
+                .reduce(0, (n, p) -> n * 10 + p);
     }
 
     static List<Integer> oddOrEven(List<Integer> integers) {
-        if (integers.stream().mapToInt(Integer::intValue).sum() % 2 == 0) {
-            return integers.stream().filter(t -> t % 2 == 0).collect(Collectors.toList());
-        } else {
-            return integers.stream().filter(t -> t % 2 != 0).collect(Collectors.toList());
-        }
+        return integers.stream()
+                .collect(Collectors.partitioningBy(t -> t % 2 == 0))
+                .get(integers.stream().mapToInt(Integer::intValue).sum() % 2 != 0);
     }
 }
